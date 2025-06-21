ed3a933662e98e8493ed299d47a4e4d531897049
Vizibly3 <vizibly3@gmail.com>
2025-06-21 19:26:27 +0530

    Update useDynamicSiteConfig.ts

--- src/hooks/useDynamicSiteConfig.ts
+++ src/hooks/useDynamicSiteConfig.ts
@@ -1,11 +1,10 @@
-
-import { useQuery } from '@tanstack/react-query';
-import { fetchSiteSettings } from './useSiteSettings';
-import { siteConfig } from '@/config/site';
+import { useQuery } from "@tanstack/react-query";
+import { fetchSiteSettings } from "./useSiteSettings";
+import { siteConfig } from "@/config/site";
 
 export const useDynamicSiteConfig = () => {
   const { data: dbSettings, isLoading } = useQuery({
-    queryKey: ['site_settings'],
+    queryKey: ["site_settings"],
     queryFn: fetchSiteSettings,
     refetchOnWindowFocus: false,
   });
@@ -18,6 +17,13 @@ export const useDynamicSiteConfig = () => {
     email: dbSettings?.email || siteConfig.email,
     contactPhone: dbSettings?.contactphone || siteConfig.contactPhone,
     address: dbSettings?.address || siteConfig.address,
+    response_time_stat:
+      dbSettings?.response_time_stat || siteConfig.response_time_stat,
+    rating_stat: dbSettings?.rating_stat || siteConfig.rating_stat,
+    satisfaction_stat:
+      dbSettings?.satisfaction_stat || siteConfig.satisfaction_stat,
+    happy_customers_stat:
+      dbSettings?.happy_customers_stat || siteConfig.happy_customers_stat,
   };
 
   return {
